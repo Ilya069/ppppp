607562dcfeb82b8539e3e9676f2c4b258792281a
Ilya069 <63396607+Ilya069@users.noreply.github.com>
2021-09-03 18:42:43 +0600

    Add files via upload

---
+++ clickertg/config.py
@@ -0,0 +1,6 @@
+API_TOKEN = '1283760421:AAFbLY3qc9owF-8D_sZPoIeYWhV7ENT78LM
+' # токен от вашего бота в телеграме (взять тут t.me/botfather)
+number = '77716067335' # номер киви кошелька
+QIWI_SEC_TOKEN = 'eyJ2ZXJzaW9uIjoiUDJQIiwiZGF0YSI6eyJwYXlpbl9tZXJjaGFudF9zaXRlX3VpZCI6InZxM2Zsdy0wMCIsInVzZXJfaWQiOiI3NzcxNjA2NzMzNSIsInNlY3JldCI6ImU0Zjk2YThlOWQ3M2Y4ZTJhMzg2MTQ3MTlhZGFkNjQyNjc4NTIxMmNkODFmZTZlZGM5ZGNlMDFiNTQxZDFmNDgifX0=' # секретный ключ p2p https://p2p.qiwi.com
+sum = 100 # сумма которую пользователь должен оплатить
+admin = '715292049' # id админа, узнать тут t.me/userinfobot
